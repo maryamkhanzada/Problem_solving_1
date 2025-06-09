0c7d01849af23aee3cc13c4e22ed28917d087500
maryamkhanzada <khanzadamuzammil112@gmail.com>
2025-06-09 22:51:24 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// Problem Solving Exercise 3
+
+
+1) Count Letters in a Word
+
+let word = "Hello world";
+console.log(word.length);
+
+//2) If/Else: Check if Number is Big
+
+let number = 130;
+
+if (number > 100) {
+  console.log("The number is big.");
+} else{
+  console.log("The number is not big.");
+
+//3) Loops: Count from 1 to 5
+
+for (let i = 1; i <=5; i++){
+  console.log(i);
+}
+
+//4) Remove Last Item of an Array
+
+let fruits = ["apple" , "mango" , "grapes"];
+
+fruits.pop();
+console.log(fruits);
+
